44759e3316f7fee263a16fe8081f80541a2cf5fb
Jisha-George <you@example.com>
2019-03-07 23:58:14 +0000

    update

--- 1-2-3-French/app/src/main/java/com/a13488071/a1_2_3_french/StatsPage.java
+++ 1-2-3-French/app/src/main/java/com/a13488071/a1_2_3_french/StatsPage.java
@@ -1,7 +1,12 @@
 package com.a13488071.a1_2_3_french;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class StatsPage extends AppCompatActivity {
 
@@ -9,5 +14,33 @@ public class StatsPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats_page);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigation_id);
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(2);
+        menuItem.setChecked(true);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.learn_id:
+                        Intent intent = new Intent(StatsPage.this, MainActivity.class);
+                        startActivity(intent);
+                        break;
+
+                    case R.id.quiz_id:
+                        Intent intent1 = new Intent(StatsPage.this, QuizPage.class);
+                        startActivity(intent1);
+                        break;
+
+                    case R.id.stats_id:
+
+                        break;
+                }
+
+                return false;
+            }
+        });
     }
 }
